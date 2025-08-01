3299a44a7f932e37ee3331f013044084e4cc12f8
Dade1991 <dade91@msn.com>
2025-08-01 09:40:33 +0200

    Update after debriefing

--- FS0325-epibooks-4-hw-main/src/tests/BookList.test.jsx
+++ FS0325-epibooks-4-hw-main/src/tests/BookList.test.jsx
@@ -1,13 +1,21 @@
 import { fireEvent, render, screen } from "@testing-library/react"
 import { describe, expect, it } from "vitest"
 import BookList from "../components/BookList"
+import Fantasy from "../data/fantasy.json"
 
-describe.skip("after finished promises", () => {
+describe("after finished promises", () => {
   it('checks if the searchbar is working with "witcher" word', async () => {
-    render(<BookList />)
-    const searchInput = screen.getAllByPlaceholderText("Cerca un libro")
-    fireEvent.change(searchInput, { target: { value: "Witcher" } })
-    const arrayOfFindings = await screen.findAllByTestId("singleBook")
+    render(<BookList books={Fantasy} />)
+    const searchInput = screen.getByPlaceholderText("Cerca un libro")
+    fireEvent.change(searchInput, { target: { value: "witcher" } })
+    const arrayOfFindings = await screen.findAllByTestId("card")
     expect(arrayOfFindings).toHaveLength(3)
+  })
+
+  it("checks if the selected card border highlight in RED", async () => {
+    render(<BookList books={Fantasy} />)
+    const cardSelected = await screen.findAllByTestId("card")
+    fireEvent.click(cardSelected[0])
+    expect(cardSelected[0]).toHaveStyle("border: 3px solid red")
   })
 })
--- FS0325-epibooks-4-hw-main/src/tests/SingleBook.test.jsx
+++ FS0325-epibooks-4-hw-main/src/tests/SingleBook.test.jsx
@@ -1,10 +1,11 @@
 import { fireEvent, render, screen } from "@testing-library/react"
 import { describe, expect, it } from "vitest"
 import SingleBook from "../components/SingleBook"
+import Fantasy from "../data/fantasy.json"
 
 describe.skip("after finished promises", () => {
   it("checks if the selected card border highlight in RED", async () => {
-    render(<SingleBook />)
+    render(<SingleBook book={Fantasy[0]} />)
     const cardSelected = await screen.findByTestId("card")
     fireEvent.click(cardSelected)
     expect(cardSelected).toHaveStyle("border: 3px solid red")
